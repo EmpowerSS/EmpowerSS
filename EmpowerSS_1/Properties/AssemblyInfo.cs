29839584da35901da47d3efa12e63adda959c089
stefan.schedletzky <stefan.schedletzky@E4CDEL007>
2023-03-28 14:24:52 +0200

    Projektdateien hinzufügen.

---
+++ EmpowerSS_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerSS_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerSS_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("CC32FC99-EE53-442E-80D1-13EA7C0274BE")]
+[assembly: AssemblyVersion("1.0.0.0")]
